cce0235bb098e2226db4548e063ac0bb05b9405e
pedrohrdev <pedrohrdev@gmail.com>
2026-01-12 08:57:33 -0300

    fix(routes): resolve route conflict by making year endpoint use query param and id use /id/:id path

--- projetos/05-historical-ipca-data-api/index.js
+++ projetos/05-historical-ipca-data-api/index.js
@@ -13,7 +13,7 @@ app.get('/historicoIPCA', (req, res) => {
 
 
 // The second API route should return a data history of the collection, referring to a specific year.
-app.get('/historicoIPCA/:ano', (req, res) => {
+app.get('/historicoIPCA/ano', (req, res) => {
 
     let ano = Number(req.params.ano);
 
@@ -27,6 +27,21 @@ app.get('/historicoIPCA/:ano', (req, res) => {
 
 });
 
+
+// The third API route should return an element from the collection, according to the element's identification code (id).
+app.get('/historicoIPCA/id/:id', (req, res) => {
+
+    let id = Number(req.params.id);
+
+    if(isNaN(id)) {
+        return res.status(404).json({error: 'id invalido'});
+    };
+
+    let specificItem = historicoInflacao.find(item => item.id  === id);
+
+    res.json(specificItem);
+
+})
 
 
 
