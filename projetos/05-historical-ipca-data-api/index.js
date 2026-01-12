cd16f693ce6770fad7dd5bdfc8c7fe9b58899b16
pedrohrdev <pedrohrdev@gmail.com>
2026-01-12 08:43:54 -0300

    fix(ipca): correct year-specific endpoint to properly use route param and filter monthly data

--- projetos/05-historical-ipca-data-api/index.js
+++ projetos/05-historical-ipca-data-api/index.js
@@ -12,7 +12,24 @@ app.get('/historicoIPCA', (req, res) => {
 });
 
 
+// The second API route should return a data history of the collection, referring to a specific year.
+app.get('/historicoIPCA/:ano', (req, res) => {
+
+    let ano = Number(req.params.ano);
+
+    if (isNaN(ano)) {
+        return res.status(404).json({erro: 'Ano invalido'})
+    }
+
+    let specificYear = historicoInflacao.filter(specificYear => specificYear.ano === ano);
+
+    res.json(specificYear)
+
+});
+
+
+
 
 app.listen(8080, () => {
     console.log('API runing on port 8080')
-})
+})  
